2a01c232562822059714e27589420e9fc8aa7d7c
d9nich <dan.galaiko@gmail.com>
2020-10-01 10:30:07 +0300

    Part of Animal has been created.

---
+++ src/com/d9nich/pathFindingAlgorithm/geneticAlgorithm/Animal.java
@@ -0,0 +1,54 @@
+package com.d9nich.pathFindingAlgorithm.geneticAlgorithm;
+
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class Animal {
+
+    private final int[] gene;
+    private final int attemptToLive;
+
+    public Animal(int[] gene, int attemptToLive) {
+        this.gene = gene;
+        this.attemptToLive = attemptToLive;
+    }
+
+    public int[] getGene() {
+        return gene;
+    }
+
+    public int getAttemptToLive() {
+        return attemptToLive;
+    }
+
+    public int[] makeCrossoverGene(Animal parent) {
+        if (parent.gene.length != gene.length)
+            throw new InputMismatchException("Parent gene is not the same size as other parent");
+        Set<Integer> notUsedGene = new HashSet<>();
+        IntStream.of(parent.gene).forEach(notUsedGene::add);
+        int pointOfSplit = new Random().nextInt(parent.gene.length / 2) + 1;
+
+        int[] geneOfChildAnimal = new int[parent.gene.length];
+        //Copying first part of gene
+        for (int i = 0; i < pointOfSplit; i++) {
+            geneOfChildAnimal[i] = parent.gene[i];
+            notUsedGene.remove(parent.gene[i]);
+        }
+        //Copying second part of genes
+        AtomicInteger positionInNewGene = new AtomicInteger(pointOfSplit);
+        for (int i = pointOfSplit; i < geneOfChildAnimal.length; i++) {
+            int pieceOfGene = gene[i];
+            if (notUsedGene.contains(pieceOfGene)) {
+                notUsedGene.remove(pieceOfGene);
+                geneOfChildAnimal[positionInNewGene.getAndIncrement()] = pieceOfGene;
+            }
+        }
+        //Copying missing genes
+        notUsedGene.forEach(e -> geneOfChildAnimal[positionInNewGene.getAndIncrement()] = e);
+        return geneOfChildAnimal;
+    }
+}
