fe8a1c240c9f60a75dcf454cb5726696f621d690
d9nich <dan.galaiko@gmail.com>
2020-10-03 20:11:59 +0300

    Improve of CycleCrossing.java

--- src/com/d9nich/pathFindingAlgorithm/geneticAlgorithm/crossoverStrategy/CycleCrossing.java
+++ src/com/d9nich/pathFindingAlgorithm/geneticAlgorithm/crossoverStrategy/CycleCrossing.java
@@ -1,12 +1,11 @@
 package com.d9nich.pathFindingAlgorithm.geneticAlgorithm.crossoverStrategy;
 
 import java.util.HashSet;
-import java.util.Random;
 import java.util.Set;
 import java.util.stream.IntStream;
 
 public class CycleCrossing extends CrossingStrategy {
-    private static int[] bornChild(int firstSplitPoint, int secondSplitPoint, int[] firstFather, int[] secondFather) {
+    private static int[] bornChild(int[] firstFather, int[] secondFather) {
         int[] firstChild = new int[firstFather.length];
         Set<Integer> set = new HashSet<>();
         IntStream.of(firstFather).forEach(set::add);
@@ -37,13 +36,8 @@ public class CycleCrossing extends CrossingStrategy {
 
     @Override
     public void crossAnimal() {
-        Random random = new Random();
-        int maxValue = firstFather.length;
-        final int middleOfGene = maxValue / 2;
-        int firstSplitPoint = random.nextInt(middleOfGene);
-        int secondSplitPoint = random.nextInt(middleOfGene) + middleOfGene;
         //Born of first child
-        firstChild = bornChild(firstSplitPoint, secondSplitPoint, firstFather, secondFather);
-        secondChild = bornChild(firstSplitPoint, secondSplitPoint, secondFather, firstFather);
+        firstChild = bornChild(firstFather, secondFather);
+        secondChild = bornChild(secondFather, firstFather);
     }
 }
