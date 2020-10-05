d98ff720437ce233b54187f9e89e0149e16cc651
d9nich <dan.galaiko@gmail.com>
2020-10-05 15:10:15 +0300

    Changer class has been done

---
+++ src/com/d9nich/pathFindingAlgorithm/testAlgorithm/changable/Changer.java
@@ -0,0 +1,41 @@
+package com.d9nich.pathFindingAlgorithm.testAlgorithm.changable;
+
+public class Changer {
+    private final Changeable<Object>[] array;
+    private final int NUMBER_OF_CYCLES;
+    private int position = 0;
+    private int cycleNumber = 0;
+
+
+    public Changer(Changeable<Object>[] array, int number_of_cycles) {
+        if (array.length == 0)
+            throw new IllegalArgumentException("Length of array can not be zero");
+        this.array = array;
+        NUMBER_OF_CYCLES = number_of_cycles;
+    }
+
+    public boolean hasNext() {
+        return NUMBER_OF_CYCLES != cycleNumber;
+    }
+
+    public void iterate() {
+        final Changeable<Object> changeable = array[position];
+        if (changeable.hasNext()) {
+            changeable.setTesting(true);
+            changeable.change();
+        } else {
+            changeable.setTesting(false);
+            changeable.fromStart();
+            position++;
+            if (position > array.length) {
+                position = 0;
+                cycleNumber++;
+            }
+            iterate();
+        }
+    }
+
+    public void setOptimal() {
+        array[position].setOptimal();
+    }
+}
