30bb8aa91cce1c88036c7ed479ff36d9d3564d92
d9nich <dan.galaiko@gmail.com>
2020-10-08 20:29:41 +0300

    Optimal parameters has been installed

--- src/com/d9nich/Main.java
+++ src/com/d9nich/Main.java
@@ -2,24 +2,22 @@ package com.d9nich;
 
 import com.d9nich.pathFindingAlgorithm.GreedyAlgorithm;
 import com.d9nich.pathFindingAlgorithm.geneticAlgorithm.GeneticWorld;
-import com.d9nich.pathFindingAlgorithm.geneticAlgorithm.crossoverStrategy.PartiallyMapped;
+import com.d9nich.pathFindingAlgorithm.geneticAlgorithm.crossoverStrategy.OrderCrossing;
 import com.d9nich.pathFindingAlgorithm.geneticAlgorithm.localStrategy.OnePointLocal;
 import com.d9nich.pathFindingAlgorithm.geneticAlgorithm.mutationStrategy.SingleMutation;
-import com.d9nich.pathFindingAlgorithm.geneticAlgorithm.selectionStrategy.RandomSelection;
+import com.d9nich.pathFindingAlgorithm.geneticAlgorithm.selectionStrategy.TournamentSelection;
 
 public class Main {
 
     public static void main(String[] args) {
         int[][] matrix = MatrixDistanceGenerator.generate(300);
         System.out.println(new GreedyAlgorithm(matrix).getLength());
-        GeneticWorld geneticWorld = new GeneticWorld(matrix, 50, new RandomSelection<>(), new PartiallyMapped(),
-                new SingleMutation(), new OnePointLocal(1.0 / 100_000_000), 20);
-        long startTime = System.currentTimeMillis();
-        for (int i = 0; i < 5_000 * 50; i++) {
-            if (i % 5_000 == 0)
+        GeneticWorld geneticWorld = new GeneticWorld(matrix, 70, new TournamentSelection<>(),
+                new OrderCrossing(), new SingleMutation(), new OnePointLocal(1.0 / 100_000_000), 120);
+        for (int i = 0; i < 5_000 * 1_000; i++) {
+            if (i % 40_000 == 0)
                 System.out.println("Length: " + geneticWorld.getLength());
             geneticWorld.iterate();
         }
-        System.out.println("End: " + (System.currentTimeMillis() - startTime));
     }
 }
